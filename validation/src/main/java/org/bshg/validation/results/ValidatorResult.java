c10e9ffc22ffe533eceb9cd4e3aa22afe8bad0d0
bshg <hamzabousalih3@gmail.com>
2024-07-06 21:24:25 +0100

    fix some issue

--- validation/src/main/java/org/bshg/validation/results/ValidatorResult.java
+++ validation/src/main/java/org/bshg/validation/results/ValidatorResult.java
@@ -3,14 +3,12 @@ package org.bshg.validation.results;
 import java.util.ArrayList;
 import java.util.List;
 
-public record ValidatorResult(
-        List<TypeValidateResult<?>> simple
-) {
+public record ValidatorResult(List<TypeValidateResult<?>> items) {
     public ValidatorResult() {
         this(new ArrayList<>());
     }
 
     public boolean errorFound() {
-        return this.simple.stream().anyMatch(TypeValidateResult::isNotValid);
+        return this.items.stream().anyMatch(TypeValidateResult::isNotValid);
     }
 }
